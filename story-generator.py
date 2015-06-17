00ee729041e335882c9cc28c1b0c5f5b0b6b6b07
jomogalla <domogalla3@gmail.com>
2015-06-17 09:55:00 -0700

    updated the story generator to require a story name

--- story-generator.py
+++ story-generator.py
@@ -2,12 +2,22 @@ from os import listdir
 from os.path import isfile, join
 import sys
 
-print 'Using folder: ', str(sys.argv[1])
+images_directory = './images/'
+drafts_directory = './_drafts/'
+
+# Steps to generating a story:
+# Create a folder with all your images
+# The name of this folder will be the name of your story
+# run 'python story-generator.py STORY_NAME'
+# This will generate your story in the appr
+
+if len(sys.argv) == 1:
+    sys.exit('Please provide an image directory')
+else:
+	story_name = sys.argv[1]
+	directory = images_directory + story_name
 
 print 'Using folder: ', str(directory)
-
-if len(sys.argv) == 0:
-    directory = './';
 
 files = [ f for f in listdir(directory) if isfile(join(directory,f)) ]
 
@@ -15,14 +25,14 @@ files.remove('.DS_Store')
 
 # print files
 
-generatedStoryFile = open('generatedStoryFile.markdown','wb')
+generatedStoryFile = open((drafts_directory + story_name + '.markdown'),'wb')
 generatedStoryFile.write('---\n')
 generatedStoryFile.write('layout: story\n')
-generatedStoryFile.write('slug: "trinity-two"\n')
-generatedStoryFile.write('title:  "Life in the Burnzone"\n')
-generatedStoryFile.write('description: "The second trip to Trinity"\n')
-generatedStoryFile.write('permalink: /stories/trinity-two/\n')
-generatedStoryFile.write('storynumber: 55\n')
+generatedStoryFile.write('slug: "' + story_name + '"\n')
+generatedStoryFile.write('title:  ""\n')
+generatedStoryFile.write('description: ""\n')
+generatedStoryFile.write('permalink: /stories/' + story_name + '/\n')
+generatedStoryFile.write('storynumber: \n')
 generatedStoryFile.write('---\n')
 
 for file in files:
